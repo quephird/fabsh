26d093ab532be3fe1ed3a1360a8704083fc9646a
danielle kefford <danielle_kefford@hotmail.com>
2015-01-30 17:18:20 -0500

    Got some lovely advice from a friend on how to use types as a dispatch mechanism.

--- src/fabsh.hs
+++ src/fabsh.hs
@@ -5,8 +5,27 @@ import System.Environment (setEnv)
 import System.Exit (exitSuccess)
 import System.Process (createProcess, proc, waitForProcess)
 
+data Command
+  = Cd String
+  | Exit
+  | Set String String
+  | PosixCommand String [String]
+  | Echo [String]
+  | Unknown
+
+
+exec :: Command -> IO()
+exec (Cd newDir) = setCurrentDirectory newDir
+exec Exit        = exitSuccess
+exec (Set k v)   = setEnv k v
+exec (PosixCommand cmd args) = do
+                                 (_, _, _, procHandle) <- createProcess $ proc cmd args
+                                 exitCode <- waitForProcess procHandle
+                                 return ()
+exec _  = putStrLn "I DON'T KNOW WHAT THAT MEANS!"
+
+
 -- TODO: Improve error handling.
---       Move logic in case clauses to separate functions.
 --       Figure out how to implement command recall.
 --       ls isn't quite working properly; probably problem with parsing of args
 shell :: IO()
@@ -14,22 +33,16 @@ shell =
   do
     putStr "💄 👗 👠 💅 >"
     commandAndArgs <- getLine
-    let command:args = words commandAndArgs in
+    let cmd:args   = words commandAndArgs
+        fArg:rArgs = args
+        k:v:ignore = splitOn "=" fArg in
       do
-        case command of
-          "cd"   -> do
-                      setCurrentDirectory $ head args
-                      shell
-          "exit" -> do
-                      exitSuccess
-          "set"  -> do
-                      let k:v:ignore = splitOn "=" $ head args
-                      setEnv k v
-                      shell
-          _      -> do
-                      (_, _, _, procHandle) <- createProcess $ proc command args
-                      waitForProcess procHandle
-                      shell
+        case cmd of
+          "cd"   -> exec $ Cd fArg
+          "exit" -> exec Exit
+          "set"  -> exec $ Set k v
+          _      -> exec $ PosixCommand cmd args
+        shell
 
 main = do
   putStrLn "Welcome to the Fab Shell!!!"
