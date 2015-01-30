6535b0936e6bfdec60a87032e283a995a2ab0033
danielle kefford <danielle_kefford@hotmail.com>
2015-01-30 11:49:50 -0500

    Added some TODOs, some code cleanup.

--- src/fabsh.hs
+++ src/fabsh.hs
@@ -5,6 +5,10 @@ import System.Environment (setEnv)
 import System.Exit (exitSuccess)
 import System.Process (createProcess, proc, waitForProcess)
 
+-- TODO: Improve error handling.
+--       Move logic in case clauses to separate functions.
+--       Figure out how to implement command recall.
+--       ls isn't quite working properly; probably problem with parsing of args
 shell :: IO()
 shell =
   do
@@ -23,7 +27,7 @@ shell =
                       setEnv k v
                       shell
           _      -> do
-                      (_, _, _, procHandle) <- createProcess (proc command args)
+                      (_, _, _, procHandle) <- createProcess $ proc command args
                       waitForProcess procHandle
                       shell
 
